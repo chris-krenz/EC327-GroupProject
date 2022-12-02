047956f6c05984587ffca8174e34156091b8e6a7
chris-krenz <chris.dean.krenz@gmail.com>
2022-12-02 04:00:51 -0500

    initial commit

---
+++ src/Wheel1.gd
@@ -0,0 +1,24 @@
+extends AnimatedSprite
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+var toggle = false
+
+
+# Called when the node enters the scene tree for the first time.
+#func _ready():
+#	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_TestButton_pressed():
+	if playing == false:
+		playing = true
+	else:
+		playing = false
