b94e6161dd0053b7c021c74bfad823e5d65b417c
chris-krenz <chris.dean.krenz@gmail.com>
2022-12-07 23:59:00 -0500

    just Wheel1.gd

--- src/Wheel1.gd
+++ src/Wheel1.gd
@@ -1,32 +1,18 @@
 extends AnimatedSprite
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-var toggle = false
+var rng : RandomNumberGenerator = RandomNumberGenerator.new()
+var timer : int
 
 
-# Called when the node enters the scene tree for the first time.
-#func _ready():
-#	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
-
-func _on_TestButton_pressed():
+# TODO: Redo animation frame rate so each each side has a frame squarely aligned, with consistent spacing
+func _on_Lever_pulled():
 	if playing == false:
+		timer = rng.randi_range(0, 100) + 100
 		playing = true
-	else:
-		playing = false
 
 
-func _on_TestButton_button_down():
-	pass # Replace with function body.
-
-
-func _on_TestButton_toggled(button_pressed):
-	pass # Replace with function body.
+func _process(_delta):
+	if playing == true:
+		timer -= 1
+		if timer == 0:
+			playing = false
