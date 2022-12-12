931fb1043453f3c70169940947939cf3f455a33e
chris-krenz <chris.dean.krenz@gmail.com>
2022-12-12 03:47:03 -0500

    other tweaks; just need sound

--- src/Scripts/Slot_Title_Menu.gd
+++ src/Scripts/Slot_Title_Menu.gd
@@ -6,10 +6,12 @@ func _ready():
 	for button in $Container.get_children():
 		button.connect("pressed", self, "_on_button_pressed", [button.scene_to_load])
 
+
 func _on_button_pressed(scene_to_load):
 	scene_path_to_load = scene_to_load
 	$FadeIn.show()
 	$FadeIn._transition()
+
 
 func _on_FadeIn_transitioned():
 	get_tree().change_scene(scene_path_to_load)	# Warning: Returns unused val: OK
