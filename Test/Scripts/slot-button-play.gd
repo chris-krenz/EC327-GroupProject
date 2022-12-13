78b2de1b8ffecf2eb6f0cea35f704f72d0238350
chris-krenz <chris.dean.krenz@gmail.com>
2022-12-12 23:17:43 -0500

    FINAL COMMIT

--- Test/Scripts/slot-button-play.gd
+++
@@ -1,9 +0,0 @@
-extends Button
-
-
-func _ready():
-	pass
-
-
-func _on_slotbuttonplay_pressed():
-	get_tree().change_scene("res://SlotMachine.tscn")
--- Test/Scripts/slot-mainmenu.gd
+++
@@ -1,9 +0,0 @@
-extends Button
-
-
-func _ready():
-	pass
-
-
-func _on_slotmainmenu_pressed():
-	get_tree().change_scene("res://Slot_Title_Menu.tscn")
